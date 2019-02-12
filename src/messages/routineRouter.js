300168c7413c730bf5e181f59ec0f0c8759e2350
Daniel Llopis <dan.llopis96@gmail.com>
2019-02-12 12:18:27 +0100

    Fixes merge errors

--- src/messages/routineRouter.js
+++ src/messages/routineRouter.js
@@ -6,7 +6,7 @@ allCommands = allCommands.concat(require('./routines/role').commands);
 allCommands = allCommands.concat(require('./routines/system').commands);
 allCommands = allCommands.concat(require('./routines/util').commands);
 allCommands = allCommands.concat(require('./routines/fun').commands);
-allCommands = allRoutines.concat(require('./routines/lol'));
+allCommands = allCommands.concat(require('./routines/lol').commands);
 
 module.exports = {
     allCommands,
--- src/messages/routines/lol.js
+++ src/messages/routines/lol.js
@@ -2,9 +2,9 @@
 const axios = require('axios');
 const leagueApi = 'RGAPI-75f0f018-4f9e-4cfe-ac21-292e0c5ed1db';
 
-module.exports =
+module.exports = {
 
-    [
+    commands: [
         {
             name: 'summoner',
             type: 'COMMAND',
@@ -18,8 +18,8 @@ module.exports =
             exec: async sentMsg => {
                 var args = sentMsg.content.split(' ');
                 var summonerSearch = args[1];
-                for(var i = 2; i<args.length;i++){
-                    summonerSearch+=`%20${args[i]}`;
+                for (var i = 2; i < args.length; i++) {
+                    summonerSearch += `%20${args[i]}`;
                 }
                 var summonerID;
                 var summonerName;
@@ -31,29 +31,32 @@ module.exports =
 
                 var responseSummoner = await axios.get(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerSearch}?api_key=${leagueApi}`);
                 summonerID = responseSummoner.data.id;
-                summonerName = responseSummoner.data.name;                
+                summonerName = responseSummoner.data.name;
                 summonerlv = responseSummoner.data.summonerLevel;
 
                 var responseLeague = await axios.get(`https://euw1.api.riotgames.com/lol/league/v4/positions/by-summoner/${summonerID}?api_key=${leagueApi}`);
-            
+
                 console.log(responseLeague.data)
-                if(responseLeague.data.length != 0){
+                if (responseLeague.data.length != 0) {
                     summonerLeague = responseLeague.data[0].tier;
                     summonerRank = responseLeague.data[0].rank;
                     summonerWR = `${(((responseLeague.data[0].wins) / (responseLeague.data[0].wins + responseLeague.data[0].losses)) * 100).toFixed(2)}%`;
-                }                
-                
+                }
+
                 var msg = `Nombre: ${summonerName}, 
                   Nivel: ${summonerlv}, 
                   Winrate: ${summonerWR}, 
                   Liga: ${summonerLeague}, 
                   Rango: ${summonerRank}`;
 
-                  if(summonerLeague === 'IRON')
+                if (summonerLeague === 'IRON')
                     msg += `\n${ironquote}`;
 
                 sentMsg.reply(msg);
             },
             errorMsg: ''
         }
-    ];
+
+    ],
+    reacts: []
+};
