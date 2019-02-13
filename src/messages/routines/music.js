64b729ce021810f1eb99cad0f9bbf78126c1ede3
Daniel Llopis <dan.llopis96@gmail.com>
2019-02-13 12:19:34 +0100

    Adds stop command a fixes some errors

--- src/messages/routines/music.js
+++ src/messages/routines/music.js
@@ -20,25 +20,45 @@ module.exports = {
             exec: sentMsg => {
                 console.log(sentMsg.args);
                 const url = sentMsg.args;
-                if (!sentMsg.guild.voiceConnection) {
+                var paused = false;
+                if (!sentMsg.guild.voiceConnection || !paused) {
                     const streamOptions = { seek: 0, volume: 1 };
                     var voiceChannel = sentMsg.member.voiceChannel;
                     voiceChannel.join().then(connection => {
                         console.log("joined channel");
                         const stream = youtube(url, { filter: 'audioonly' });
                         const dispatcher = connection.playStream(stream, streamOptions);
-                        dispatcher.on("end", end => {
-                            console.log("left channel");
-                            voiceChannel.leave();
-                        });
+                        
                     }).catch(err => console.log(err));
                 }
-                var paused = false;
                 console.log(url)
                 sentMsg.channel.send('Reproduciendo.')
             },
             errorMsg: ''
+        },
+        {
+            name: 'stop',
+            type: 'COMMAND',
+            description: 'Reproduce un video dada una url.',
+            command: '$stop',
+            priority: 1,
+            condition: sentMsg => {
+                var args = sentMsg.content.split(' ');
+                return args[0] === '$stop';
+            },
+            exec: sentMsg => {
+                console.log(sentMsg.args);
+                const url = sentMsg.args;
+                var paused = false;
+                if (sentMsg.guild.voiceConnection || !paused) {
+                    var voiceChannel = sentMsg.member.voiceChannel;
+                    voiceChannel.leave();
+                }
+                sentMsg.channel.send('Parando.')
+            },
+            errorMsg: ''
         }
+
     ],
 
 
